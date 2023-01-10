671587d691e84bc7dd6519e656523e6d7fe912d0
JJTK-code <335235@student.mboutrecht.nl>
2023-01-10 12:24:31 +0100

    Final commit

---
+++ db/dureauto.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 10, 2023 at 11:22 AM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php-pdo-crud-toets`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dureauto`
+--
+
+DROP TABLE IF EXISTS `dureauto`;
+CREATE TABLE IF NOT EXISTS `dureauto` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Merk` varchar(200) NOT NULL,
+  `Model` varchar(200) NOT NULL,
+  `Topsnelheid` smallint(6) NOT NULL,
+  `Prijs` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
